d290106402deeff2b15fa89531edf46ebe96e158
Bymbaajav Ganbold <ganboldbymbaaaaa@example.com>
2026-02-12 11:03:45 +0800

    asdf

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	env: {
-		NEXT_PUBLIC_API_URL: "http://ottapp.ecm.mn/api",
+		NEXT_PUBLIC_API_URL: "https://ottapp.ecm.mn/api",
 		NEXT_PUBLIC_OTP_API_URL: "https://api-message.ecm.mn",
 	},
 	images: {
